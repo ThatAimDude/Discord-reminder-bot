4e2a14dc46e21afedaf2ea43279056570ae3fff5
ThatAimDude <46623193+ThatAimDude@users.noreply.github.com>
2023-06-11 17:56:08 +0200

    Added buttons and made more usable

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from discord.ext import commands
 import discord
 from asyncio import sleep
@@ -13,45 +12,105 @@ bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 today_date = dt.now()
 
 
+class Menu(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.option = None
+
+
+    @discord.ui.button(label='Sprawdzian', style=discord.ButtonStyle.primary)
+    async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.option = 'Sprawdzian'
+        self.value = '1'
+        self.stop()
+        await interaction.response.defer()
+
+
+    @discord.ui.button(label='Kartkowka', style=discord.ButtonStyle.primary)
+    async def menu2(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.option = 'Kartkowka'
+        self.value = '2'
+        self.stop()
+        await interaction.response.defer()
+
+    @discord.ui.button(label='Praca Domowa', style=discord.ButtonStyle.primary)
+    async def menu3(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.option = 'Praca Domowa'
+        self.value = '3'
+        self.stop()
+        await interaction.response.defer()
+
+    @discord.ui.button(label='Project', style=discord.ButtonStyle.primary)
+    async def menu4(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.option = 'Project jebany'
+        self.value = '4'
+        self.stop()
+        await interaction.response.defer()
+
+    @discord.ui.button(label='Custom', style=discord.ButtonStyle.primary)
+    async def menu5(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = '5'
+        self.stop()
+        await interaction.response.defer()
+
+    async def wait_for_response(self):
+        await self.wait()
+        return self.value
+
+
 @bot.command()
 async def remind(ctx):
     await ctx.send("When do you want to be reminded?")
     remind_date = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
     remind_date_str = remind_date.content
     await ctx.send(f"Okay, I will remind you at {remind_date_str}! What do you want me to remind you about?")
-    reminder = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+    view = Menu()
+    view.menu1
+    view.menu2
+    view.menu3
+    view.menu4
+    view.menu5
 
-    if reminder.content.lower() in ['sprawdzian', 'kartkowka','praca domowa', 'homework','test']:
-        global subject
-        global details
-        await ctx.send(f"What subject? ")
-        subject = await bot.wait_for('message',check=lambda message: message.author == ctx.author)
+    await ctx.send(view=view)
+    response = await view.wait_for_response()
+
+    list = ['1','2','3','4']
+    if response in list:
+
+        await ctx.send(f"What subject?")
+        subject = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
         await ctx.send(f"What is it about?")
-        details = await bot.wait_for('message',check=lambda message: message.author == ctx.author)
+        details = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
 
     else:
-        subject = ''
-        details = ''
+        await ctx.send("What do you want to be reminded about?")
+        reminder = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+        await ctx.send("Any details")
+        extra_info = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
 
-    await ctx.send(f"I will remind you at {remind_date_str} about {reminder.content}")
+    await ctx.send(f"I will remind you at {remind_date_str}")
 
     date_object = dt.strptime(remind_date_str, '%d-%m-%Y-%H-%M')
     date1 = (today_date - date_object) * (-1)
     date_without_time = date_object.date()
 
     embed = discord.Embed(
-        colour=discord.Colour.dark_teal(),
-        description="Some shit you have to do",
-        title="Reminder Kurwa"
+        colour=discord.Colour.dark_red(),
+        title="Reminder",
+        description="Some shit you need to do"
     )
 
-    embed.add_field(name='Remind date', value=remind_date_str, inline=False)
-    try:
-        embed.insert_field_at(1, name=subject.content, value=details.content)
-    except Exception as e:
-        embed.insert_field_at(1, name=reminder.content, value='')
+    if response in ['1', '2', '3', '4']:
+        embed.insert_field_at(1, name=f"{subject.content} {view.option} ", value=details.content)
+        option = 1
+    else:
+        embed.insert_field_at(1, name=reminder.content, value=extra_info.content)
+
+
+
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1086270045853847592/1092742274275872808/Daco_4228969.png")
-    embed.set_footer(text="This is a message from annonymous")
+    embed.set_footer(text=f"Reminder date {remind_date_str}")
+
 
     while True:
         print(date1.total_seconds())
